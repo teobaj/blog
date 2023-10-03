76b250fb8808298c5d933ba3cfba9958cc901074
teobaj <teobaj2309@gmail.com>
2023-10-03 12:09:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blog
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-gc8lzv)
---
+++ blog/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.postcss';
+	import { AppBar } from '@skeletonlabs/skeleton';
+
+</script>
+
+<AppBar/>
+<slot />
