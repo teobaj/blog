40b07a0e21128c0f9ec97f99aba67bcc3f017bd3
teobaj <teobaj2309@gmail.com>
2023-10-05 11:18:18 +0300

    add categories page

---
+++ blog/src/routes/categories/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { InputChip } from '@skeletonlabs/skeleton';
+  let list: string[] = ['foo', 'bar', 'fizz', 'buzz'];
+</script>
+<div class="p-2 flex flex-col">
+  <InputChip bind:value={list} name="chips" placeholder="Enter any value..." />
+  <button on:click={() => {list = [...list, 'item']}}>click</button>
+</div>
